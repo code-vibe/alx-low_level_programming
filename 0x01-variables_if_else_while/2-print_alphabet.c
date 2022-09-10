720f7de5f1def949ec7a86ee7a176f9909f1a43e
code-vibe <mrsamoladapo@gmail.com>
2022-09-09 21:14:33 -0700

    Prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lower case
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
