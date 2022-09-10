1a40db18ff8f7cc87f6fe2f0b02d554b65a6d44f
code-vibe <mrsamoladapo@gmail.com>
2022-09-09 21:23:36 -0700

    Prints an output

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase, then uppercase"
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'x';
+	char d = 'X';
+	while (c <= 'y')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Y')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
