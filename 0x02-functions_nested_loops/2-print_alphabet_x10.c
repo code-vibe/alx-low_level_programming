53d4c264544382bdc25471fcc833572d28f31e46
code-vibe <mrsamoladapo@gmail.com>
2022-09-13 08:26:20 -0700

    prints 10 times the alphabet😍😤

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/*
+ * File:2-print_alphabet_x10.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+#include "main.h"
+/**
+ * main - This code writes a function 
+ * 	that prints 10 times the
+ * 	alphabet in lowercase
+ * Return: 0 on success
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
