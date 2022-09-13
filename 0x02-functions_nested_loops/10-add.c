6e27304607aaac294ea30d89d085c9c7f1909e2e
code-vibe <mrsamoladapo@gmail.com>
2022-09-13 10:56:09 -0700

    adds two integers and returns the result😭🙄

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * File: 10-add.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+/**
+ * add - adds two numbers together.
+ * @x: The first number to be added.
+ * @y: The second number to be added.
+ * Return: the sum of two numbers.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
