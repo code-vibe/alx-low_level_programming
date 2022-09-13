134dae3597be5d5d8e460a94e086d3f6c0801aa3
code-vibe <mrsamoladapo@gmail.com>
2022-09-13 09:50:50 -0700

    checks for alphabetic character🥳🥶

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,12 @@
-/**
+/*
  * File: 4-isalpha.c
  * Auth: Ebenezer Sam-Oladapo
  */
+
 #include "main.h"
 /**
- * _isalpha -This program checks for
- *		alphabetic character
+ * _isalpha - this program checks wether a character
+ *            is from the English alphabet.
  * @c: input parameter
  * Return: 1 for English Character
  *         0 if the character is not English
