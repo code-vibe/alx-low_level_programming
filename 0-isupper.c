1208cff00f1346b706028dd10d1d5a73aa16980c
code-vibe <mrsamoladapo@gmail.com>
2022-09-15 02:45:38 -0700

    Checks for uppercase character🥶🚯

---
+++ 0-isupper.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-isupper.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+#include "main.h"
+
+/**
+ * _isupper - return 1 if parameter is uppercase alphabet
+ *@c: the character to check
+ * Return:  0 (fails)
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+
+	if (i == c)
+	{
+		return (1);
+	}
+	}
+
+	return (0);
+}
