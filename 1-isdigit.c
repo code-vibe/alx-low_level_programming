23ab6acb5587033bd2358d2cfb7a8685aea83527
code-vibe <mrsamoladapo@gmail.com>
2022-09-15 02:54:28 -0700

    Checks for a digit (0 through 9).♿🚹

---
+++ 1-isdigit.c
@@ -0,0 +1,33 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - return 1 if parameter is isdigit
+ * @c: the character to check
+ * Return:  0 (failure)
+ */
+
+int _isdigit(int c)
+{
+
+	int i;
+
+
+	for (i = '0'; i <= '9'; i++)
+	{
+
+		if (i == c)
+		{
+			return (1);
+		}
+	}
+
+
+	return (0);
+
+}
