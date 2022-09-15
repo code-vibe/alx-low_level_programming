0b8f4888fa0fa967dd250878734df2c731408c77
code-vibe <mrsamoladapo@gmail.com>
2022-09-15 03:00:34 -0700

    Multiplies two integers.⛰️🌏

---
+++ 2-mul.c
@@ -0,0 +1,21 @@
+/*
+ * File: 2-mul.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - return multiplication of a * b
+ * @a: first number to multiply
+ * @b: secondary number to multiply
+ * Return: multiplication of a * b
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
