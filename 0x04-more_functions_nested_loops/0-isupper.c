6fb3095db54c1c77c69e3b73e4b02b2c53e0c22b
code-vibe <mrsamoladapo@gmail.com>
2022-09-16 04:07:59 -0700

    Checks for uppercase character.😍🍑

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,10 @@
-/*
- * File: 0-isupper.c
- * Auth: Ebenezer Sam-Oladapo
- */
-
 #include "main.h"
- /**
-  * _isupper - return 1 if parameter is uppercase alphabet
-  * @c: the character to check
-  * Return:  0 (failure)
-  */
+
+/**
+ * _isupper - return 1 if parameter is uppercase alphabet
+ * @c: the character to check
+ * Return:  0 (failure)
+ */
 
 int _isupper(int c)
 {
