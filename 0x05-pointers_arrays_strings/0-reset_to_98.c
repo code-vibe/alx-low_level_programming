7d9822b6fdb4aa108fa43335f38c7c3d2c61e950
code-vibe <mrsamoladapo@gmail.com>
2022-09-19 10:57:23 -0700

    Use a pointer to change integer value🧐🎙️

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - use a pointer to change integer value
+ * @n: parameter
+ * Description - reset_to_98 resets the value of *n to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
