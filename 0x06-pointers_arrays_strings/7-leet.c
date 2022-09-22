e3846b4bad68d959a64bcced4781ad5ddb07d7b5
code-vibe <mrsamoladapo@gmail.com>
2022-09-22 03:11:21 -0700

    7-leet.c: Added a function- that encodes a string into 1337⏹️🕶️

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+/*
+ * File: 7-leet.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+
+/**
+ * leet - Encodes a string in leet
+ *
+ * @str: String to be encoded
+ *
+ * Return: Always 0.
+ */
+char *leet(char *str)
+{
+	int i = 0, k;
+	char s[] = "aAeEoOtTlL";
+	char s1[] = "4433007711";
+
+	for (; str[i] != '\0'; i++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			if (s[k] == str[i])
+			{
+				str[i] = s1[k];
+			}
+		}
+	}
+	return (str);
+}
