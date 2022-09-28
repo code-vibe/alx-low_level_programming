d1570ab3d8fad782f254342ec5b82015a95a3e68
code-vibe <mrsamoladapo@gmail.com>
2022-09-28 09:49:06 -0700

    Added function-that returns the factorial of a given number😇🇦🇼

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-factorial.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+#include "main.h"
+/**
+ * factorial - finds factorial
+ * @n: int
+ * Return: int
+ */
+
+int factorial(int n)
+{
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+
+}
