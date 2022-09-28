4f325e94df6dfd53105342887eb7ff55bb2da6e7
code-vibe <mrsamoladapo@gmail.com>
2022-09-28 08:30:01 -0700

    0-puts_recursion.c: Added a function-that prints a string, followed by a new line🧐😇

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-puts_recursion.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+
+
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+}
