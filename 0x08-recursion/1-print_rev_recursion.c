cb27276eb449a5f601d8e3170b39a41db38ca594
code-vibe <mrsamoladapo@gmail.com>
2022-09-28 08:32:48 -0700

    1-print_rev_recursion.c-Added a function-that prints a string in reverse😇👚

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+#include "main.h"
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to the string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
