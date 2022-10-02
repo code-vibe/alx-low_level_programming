006f83bedefcf1163c4c24346f2b674cfd9f1515
code-vibe <mrsamoladapo@gmail.com>
2022-10-02 05:58:45 -0700

    Added a file😇🍑

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-memset.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+/**
+ * *_memset - concatenates two strings
+ * @s:  pointer
+ * @b: constant byte
+ * @n: n bytes of memory
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0 ; i++, n--)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
