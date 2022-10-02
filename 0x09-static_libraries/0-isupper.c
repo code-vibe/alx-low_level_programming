091a2e6e1deb29cb675ef1d11f955c18181ea094
code-vibe <mrsamoladapo@gmail.com>
2022-10-02 05:57:15 -0700

    Added a file😇🍑

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - return 1 if parameter is uppercase alphabet
+ * @c: the character to check
+ * Return:  0 (failure)
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+
+	if (i == c)
+	{
+		return (1);
+	}
+	}
+
+	return (0);
+}
