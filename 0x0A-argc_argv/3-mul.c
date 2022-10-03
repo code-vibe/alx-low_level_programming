0b3152a39576af93ec91fcf156393a74fcaf25bf
code-vibe <mrsamoladapo@gmail.com>
2022-10-03 08:27:34 -0700

    3-mul.c: A program- that multiplies two numbers😇🤨

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - Write a program that multiplies two numbers
  * Your program should print the result of the multiplication,
  * followed by a new line
- * You can assume that the two numbers and result of the 
+ * You can assume that the two numbers and result of the
  * multiplication can be stored in an integer
  *
  * @argc: Argument count
