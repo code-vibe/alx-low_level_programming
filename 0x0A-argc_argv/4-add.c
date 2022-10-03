7721b39627da2effad7d36fb750b728922a4474a
code-vibe <mrsamoladapo@gmail.com>
2022-10-03 09:10:04 -0700

    4-add.c: A program that adds positive numbers🍑🌏🟠

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,7 +3,14 @@
 #include <ctype.h>
 
 /**
- * main - sum of  the args
+ * main - Write a program that adds positive numbers
+ * Print the result, followed by a new line
+ * If no number is passed to the program, print 0, followed by a new line
+ * If one of the number contains symbols that are not digits,
+ * print Error, followed by a new line, and return 1
+ * You can assume that numbers and the addition of all
+ * the numbers can be stored in an int
+ *
  * @argc: Argument count
  * @argv: Array of argument strings
  *
@@ -15,25 +22,28 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int sum = 0;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
+	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(*argv[i]))
+			int b;
+			char *str;
+
+			str = argv[i];
+			for (b = 0; str[b] != '\0'; b++)
 			{
-				printf("Error\n");
-			}
-			else
-			{
-				sum += atoi(argv[i]);
+				if (str[b] < 48 || str[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
 		}
-		printf("%d\n", sum);
 	}
-
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
