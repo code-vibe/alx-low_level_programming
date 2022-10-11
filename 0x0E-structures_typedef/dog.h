8394fab0310d11b65c688caa226969d667a2be3a
code-vibe <mrsamoladapo@gmail.com>
2022-10-11 11:49:52 -0700

    Initial commit😇🧐

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - new struct for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Return: true
+ */
+
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
