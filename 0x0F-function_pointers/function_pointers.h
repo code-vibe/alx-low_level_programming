1ea42f96301f6158c478fa012446e665a160d331
code-vibe <mrsamoladapo@gmail.com>
2022-10-12 20:13:49 -0700

    Changed file location 🤲👐

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef MAINN_H
+#define MAINN_H
+#include <stddef.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
