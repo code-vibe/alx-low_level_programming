4600ba6489bec347ee310cd050a47b253ac65bca
code-vibe <mrsamoladapo@gmail.com>
2022-10-13 21:22:45 -0700

    Add header file😇🧐

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H*/
