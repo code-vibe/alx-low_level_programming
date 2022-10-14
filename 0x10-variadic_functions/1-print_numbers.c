479ee6c526ed8aafaaa09bba5907e02622c07de4
code-vibe <mrsamoladapo@gmail.com>
2022-10-13 21:36:04 -0700

    Added a function that prints strings, followed by a new line😍😅

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,21 +13,20 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 0;
-	va_list ap;
+	unsigned int i;
+	va_list numbers;
 
-	va_start(ap, n);
-
-	if (!separator)
-		return;
+	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", va_arg(ap, int), i != (n - 1) ? separator : "");
+		printf("%d%s", va_arg(numbers, int));
+		if (i < n - 1 && seperator != NULL)
+			printf("%s", seperator);
 	}
 
 
-	va_end(ap);
-	putchar('\n');
+	va_end(numbers);
+	printf('\n');
 
 }
